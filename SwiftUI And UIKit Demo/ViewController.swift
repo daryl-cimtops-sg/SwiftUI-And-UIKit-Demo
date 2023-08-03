f8c36bf6a0f534761f56e4843826f9cd52e5dc50
daryl-cimtops-sg <daryl@cimtops-sglab.com>
2023-08-03 13:30:49 +0800

    Initial Commit

---
+++ SwiftUI And UIKit Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI And UIKit Demo
+//
+//  Created by Daryl Gialolo on 3/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
